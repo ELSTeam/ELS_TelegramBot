37b9afa587265f50cffddf9fde82bf2c7fedc98c
Omer Aplatony <omerap12@gmail.com>
2023-04-20 19:02:20 +0300

    Sign in to server work

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -25,7 +28,7 @@ type User struct {
 
 var users = make(map[int64]*User)
 
-// server_url := "http://localhost:5000"
+var server_url = "http://localhost:5000"
 
 func main() {
 	fmt.Println("Getting the APi token from os.env")
@@ -37,7 +40,7 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
-	bot.Debug = true
+	// bot.Debug = true
 	fmt.Println("Bot is up and running")
 
 	//setting up channel updates (open a channel and get updates)
@@ -74,6 +77,7 @@ func main() {
 				}
 			}
 		case WaitingForUsername:
+			user.Username = update.Message.Text
 			user.State = WaitingForPassword
 			msg := tgbotapi.NewMessage(userID, "Please enter your password:")
 			_, err := bot.Send(msg)
@@ -81,9 +85,13 @@ func main() {
 				log.Println("Error sending message:", err)
 			}
 		case WaitingForPassword:
+			user.Password = update.Message.Text
 			// connect to the server and check for credential.
-			connected := true
-			if connected == true {
+			connected, err := checkLogin(user.Username, user.Password)
+			if err != nil {
+				log.Panic(err)
+			}
+			if connected == 200 {
 				user.State = Connected
 				msg := tgbotapi.NewMessage(userID, "Connected.")
 				_, err := bot.Send(msg)
@@ -100,4 +108,44 @@ func main() {
 			}
 		}
 	}
+}
+
+func checkLogin(username string, password string) (int, error) {
+	log.Println("checklogin")
+	log.Println(username)
+	log.Println(password)
+	// Create a struct representing the JSON payload
+	payload := struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}{
+		Username: username,
+		Password: password,
+	}
+
+	// Encode the payload into a JSON string
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		return 0, err
+	}
+
+	// Create a new HTTP POST request with the payload as the request body
+	req, err := http.NewRequest("POST", server_url+"/sign_in", bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		return 0, err
+	}
+
+	// Set the request header to specify that the payload is in JSON format
+	req.Header.Set("Content-Type", "application/json")
+
+	// Send the HTTP request using the default HTTP client
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	// Return the status code of the HTTP response
+	return resp.StatusCode, nil
 }
